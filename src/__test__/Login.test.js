3ff5aaba77aace477b9f954ae96ca1286e9b486c
koyaaoyama <aoyama.ko0920@gmail.com>
2022-11-10 23:27:54 +0900

    add email validation test, password test, form submit test.

--- src/__test__/Login.test.js
+++ src/__test__/Login.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react"
-import Login from "../Login"
+import userEvent from "@testing-library/user-event"
+import Login, { validateEmail } from "../Login"
 
 // jestでテストする
 // jsetはsetupTests.jsでimportしているのでいちいちimportしなくてもいい
@@ -11,5 +12,36 @@ describe('Test Login Component', () => {
 		render(<Login />)
 		const button = await screen.findAllByRole('button')
 		expect(button).toHaveLength(1)
+	})
+
+	test('should be failed on email validation', () => {
+		const testEmail = 'example.com'
+		expect(validateEmail(testEmail)).not.toBe(true)
+	})
+
+	test('should be passed on email validation', () => {
+		const testEmail = 'example@example.com'
+		expect(validateEmail(testEmail)).toBe(true)
+	})
+
+	test('password input should have type password', () => {
+		render(<Login />)
+		const password = screen.getByPlaceholderText('パスワード入力')
+		expect(password).toHaveAttribute('type', 'password')
+	})
+
+	test('should be able to submit the form', () => {
+		render(<Login />)
+		// data-testidでgetする
+		const submitButton = screen.getByTestId('submit')
+		const email = screen.getByPlaceholderText('メールアドレス入力')
+		const password = screen.getByPlaceholderText('パスワード入力')
+
+		userEvent.type(email, 'example@example.com')
+		userEvent.type(password, 'abcdef')
+		userEvent.click(submitButton)
+
+		const userInfo = screen.getByText('example@example.com')
+		expect(userInfo).toBeInTheDocument(userInfo)
 	})
 })
